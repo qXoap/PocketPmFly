cd845a44fde32647e537e139410e4041673c420f
SxxCodzx <tbralex7@gmail.com>
2022-08-20 11:47:11 -0500

    Add new Features

--- src/PocketPmFly/EventListener.php
+++ src/PocketPmFly/EventListener.php
@@ -5,6 +5,7 @@ namespace PocketPmFly;
 use pocketmine\player\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityTeleportEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use PocketPmFly\Main;
 
 class EventListener implements Listener {
@@ -15,6 +16,21 @@ class EventListener implements Listener {
             if(Main::getInstance()->getConfig()->get("remove-fly-when-changing-world") === true){
                 $entity->setFlying(false);
                 $entity->setAllowFlight(false);
+            }
+        }
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $ev){
+        $player = $ev->getPlayer();
+        if(Main::getInstance()->getConfig()->get("activate-fly-of-a-player-when-joining") === true){
+            if($player->hasPermission("pocketfly.cmd")){
+                $player->setFlying(true);
+                $player->setAllowFlight(true);
+            }
+        }else if(Main::getInstance()->getConfig()->get("activate-fly-of-a-player-when-joining") === false){
+            if($player->hasPermission("pocketfly.cmd")){
+                $player->setFlying(false);
+                $player->setAllowFlight(false);
             }
         }
     }
