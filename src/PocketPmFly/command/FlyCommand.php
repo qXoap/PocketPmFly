fa105933098ecbe17ed19051085521d8d5f5fa04
SxxCodzx <tbralex7@gmail.com>
2022-08-19 23:56:12 -0500

    Add New Function

--- src/PocketPmFly/command/FlyCommand.php
+++ src/PocketPmFly/command/FlyCommand.php
@@ -2,6 +2,7 @@
 
 namespace PocketPmFly\command;
 
+use Forms\FormAPI\SimpleForm;
 use pocketmine\player\Player;
 use pocketmine\Server;
 use pocketmine\command\Command;
@@ -49,5 +50,37 @@ class FlyCommand extends Command {
             $player->setAllowFlight(false);
             $player->sendMessage(TextFormat::colorize($prefix.$desactive));
         }
+    }
+
+    public function getForm(Player $player){
+        $menu = new SimpleForm(function (Player $player, int $data = null){
+            if($data === null){
+                return true;
+            }
+            $prefix = Main::getInstance()->getConfig()->get("prefix");
+            $active = Main::getInstance()->getConfig()->get("fly-active-message");
+            $desactive = Main::getInstance()->getConfig()->get("fly-desactive-message");
+            $noperms = Main::getInstance()->getConfig()->get("no-permission-message");
+            switch($data){
+                case 0:
+                    $player->setFlying(true);
+                    $player->setAllowFlight(true);
+                    $player->sendMessage(TextFormat::colorize($prefix.$active));
+                break;
+                case 1:
+                    $player->setFlying(false);
+                    $player->setAllowFlight(false);
+                    $player->sendMessage(TextFormat::colorize($prefix.$desactive));
+                break;
+                case 2:
+                break;
+            }
+        });
+        $menu->addButton(TextFormat::colorize(Main::getInstance()->getConfig()->get("Form-Title")));
+        $menu->addButton(TextFormat::colorize(Main::getInstance()->getConfig()->get("Button-Fly-On")));
+        $menu->addButton(TextFormat::colorize(Main::getInstance()->getConfig()->get("Button-Fly-Off")));
+        $menu->addButton(TextFormat::colorize(Main::getInstance()->getConfig()->get("Button-Close-Form")),0,"textures/ui/redX1");
+        $player->sendForm($menu);
+        return $menu;
     }
 }
