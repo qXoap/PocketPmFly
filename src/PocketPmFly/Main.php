04ceb6387221c966be2ca1ea84365cc01930e4ca
SxxCodzx <tbralex7@gmail.com>
2022-08-19 23:30:30 -0500

    Add Command And Listener

---
+++ src/PocketPmFly/Main.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PocketPmFly;
+
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase {
+
+    public function onEnable(): void{
+        
+    }
+}
